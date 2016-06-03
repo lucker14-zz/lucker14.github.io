ce6630878b7a7c64ccbe3917d94999154089bc89
Petr Pluhar <lucker14@gmail.com>
2016-06-03 11:25:15 +0200

    changed to pixa

---
+++ main.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiOyJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
